8eb44ab977e365f673bafb0467290a132567ff19
SiteAmy <a.nijmeijer.gamedev@gmail.com>
2022-11-22 15:10:28 +0100

    Initial Commit

---
+++ Flappy Hell/Flappy_HellApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flappy_HellApp.swift
+//  Flappy Hell
+//
+//  Created by Amy Nijmeijer on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Flappy_HellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
